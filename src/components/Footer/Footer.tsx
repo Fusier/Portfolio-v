0d1ebb19c5afc126df55c82b0a407729ff621c9a
Fusier <samukoulu@gmail.com>
2025-08-11 15:15:51 -0400

    margin bugfix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer: React.FC = () => {
+  const isHomePage = location.pathname === "/" || location.pathname === "/home";
   return (
-    <footer className="footer">
+    <footer className={`footer ${!isHomePage ? "footer--with-margin" : ""}`}>
       <div className="footer__copyright">
         <p className="footer__text">
           &copy; {new Date().getFullYear()} Samu Willman. All rights reserved.{" "}
