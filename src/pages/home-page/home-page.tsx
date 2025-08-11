17b48e33cb840e6e7d206d198503a00fb4d0d657
Fusier <samukoulu@gmail.com>
2025-08-11 12:55:48 -0400

    Remove unused components, rename component

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home-page.scss";
-import TypeAnimationComponent from "../../components/TypeAnimation/TypeAnimation";
+import TypeAnimationComponent from "../../components/TypeAnimation/type-animation";
 import samuHomeImage from "../../assets/samu_hero.jpg";
 
 const HomePage: React.FC = () => {
